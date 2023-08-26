f69b2443ad4682749d5eec9082dc4777fd7f4f3c
Brian Becerra <brianbecerra617@gmail.com>
2023-08-26 12:06:02 -0500

    final

--- src/main/java/PollApi/Main.java
+++ src/main/java/PollApi/Main.java
@@ -3,13 +3,47 @@ package PollApi;
 import model.EncuestaPollDaddy;
 import model.PollDaddyClient;
 
+import java.io.IOException;
+import java.util.HashMap;
+
+import org.json.JSONObject;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        HashMap<String, Object> encuestaAttr = new HashMap<>();
 
-        PollDaddyClient client = new PollDaddyClient("105031734");
-        EncuestaPollDaddy encuesta = new EncuestaPollDaddy(client.getOpciones());
+        JSONObject pdRequest = new JSONObject();
+        JSONObject demands = new JSONObject();
+        JSONObject demand = new JSONObject();
+        JSONObject poll = new JSONObject();
+        JSONObject jsonbody = new JSONObject();
+
+        poll.put("id", "10503173");
+        demand.put("poll", poll);
+        demand.put("id", "GetPollResults");
+        demands.put("demand", demand);
+        pdRequest.put("partnerGUID", "5719aa21-c585-5aa9-42dd-00005547ef78");
+        pdRequest.put("userCode", "$P$BzM55aacFGir8JbSGtmXdZ7WGnKXiV1");
+        pdRequest.put("demands", demands);
+        jsonbody.put("pdRequest", pdRequest);
+        encuestaAttr.put("json", jsonbody);
+        //System.out.println(encuestaAttr.get("json"));
+
+        PollDaddyClient encPollDaddy = new PollDaddyClient("https://api.crowdsignal.com/v1",encuestaAttr);
+        // Establecer la conexión con el API
+        encPollDaddy.connect();
+        String requestBody = encPollDaddy.getData().get("json").toString();
+        // Enviar el objeto JSON al API
+        encPollDaddy.sendJson(requestBody);
+        // Obtener e imprimir la respuesta del API
+        String response = encPollDaddy.getResponse();
+        //System.out.println("Response: " + response);
+        // Desconectar la conexión
+        encPollDaddy.disconnect();
+
+        EncuestaPollDaddy encuesta = new EncuestaPollDaddy(encPollDaddy.getOpciones());
         encuesta.mostrarResultados();
-
     }
 
 }
+
--- src/main/java/model/PollDaddyClient.java
+++ src/main/java/model/PollDaddyClient.java
@@ -3,107 +3,126 @@ package model;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PollDaddyClient {
-    private static String idEncuesta; // ID del usuario de polldaddy
-    private static final String API_URL = "https://api.crowdsignal.com/v1";
-    private static final String PARTNER_GUID = "5719aa21-c585-5aa9-42dd-00005547ef78";
-    private static final String USER_CODE = "$P$BzM55aacFGir8JbSGtmXdZ7WGnKXiV1";
+    private HashMap<String, Object> data;
+    private URL apiUrl; // La dirección del API
+    private HttpURLConnection connection; // La conexión con el API
     private static ArrayList<Opcion> opciones;
 
 
-    public PollDaddyClient(String idEncuesta) {
-        this.idEncuesta = idEncuesta;
-        this.opciones = new ArrayList<>(); // Inicializar el ArrayList vacío
-        this.obtenerResultadosEncuesta();
+    public PollDaddyClient(String apiUrl, HashMap<String, Object> data) {
+        this.data = data;
+        try {
+            this.apiUrl = new URL(apiUrl); // Asignar la dirección del API
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+        this.opciones = new ArrayList<>();
+    }
+
+    // Declarar los métodos públicos para acceder y modificar los atributos
+
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
     }
 
     public static ArrayList<Opcion> getOpciones() {
         return opciones;
     }
 
-    public static void setOpciones(ArrayList<Opcion> opciones) {
-        PollDaddyClient.opciones = opciones;
+    public URL getApiUrl() {
+        return apiUrl;
     }
 
-    public static String getIdEncuesta() {
-        return idEncuesta;
+    public void setApiUrl(URL apiUrl) {
+        this.apiUrl = apiUrl;
     }
 
-    public static void setIdEncuesta(String idEncuesta) {
-        PollDaddyClient.idEncuesta = idEncuesta;
+    public HttpURLConnection getConnection() {
+        return connection;
     }
 
-    public static void obtenerResultadosEncuesta() {
+    public void setConnection(HttpURLConnection connection) {
+        this.connection = connection;
+    }
+
+    // Declarar un método público para establecer la conexión con el API
+    public void connect() {
         try {
-            // URL de la API
-            URL url = new URL(API_URL);
-            // Cuerpo JSON
-            String jsonBody = "{"
-                    + "    \"pdRequest\": {"
-                    + "        \"partnerGUID\": \"" + PARTNER_GUID + "\","
-                    + "        \"userCode\": \"" + USER_CODE + "\","
-                    + "        \"demands\": {"
-                    + "            \"demand\": {"
-                    + "                \"poll\": {"
-                    + "                    \"id\": \"" + idEncuesta + "\""
-                    + "                }, \"id\": \"GetPollResults\""
-                    + "            }"
-                    + "        }"
-                    + "    }"
-                    + "}";
-            // Establecer la conexión
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            // Configurar la solicitud como POST
-            connection.setRequestMethod("POST");
-            connection.setRequestProperty("Content-Type", "application/json");
-            connection.setDoOutput(true);
-            // Escribir el cuerpo JSON en la solicitud
-            OutputStream outputStream = connection.getOutputStream();
-            outputStream.write(jsonBody.getBytes());
-            outputStream.flush();
-            outputStream.close();
-            // Obtener la respuesta
-            int responseCode = connection.getResponseCode();
-            if (responseCode == HttpURLConnection.HTTP_OK) {
-                // Leer la respuesta del servidor
-                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-                String line;
-                StringBuilder response = new StringBuilder();
-                while ((line = reader.readLine()) != null) {
-                    response.append(line);
-                }
-                reader.close();
-                JSONObject json = new JSONObject(response.toString());
-                JSONObject pdResponse = json.getJSONObject("pdResponse");
-                JSONObject demands = pdResponse.getJSONObject("demands");
-                JSONArray demand = demands.getJSONArray("demand");
-                JSONObject result = demand.getJSONObject(0).getJSONObject("result");
-                JSONObject answers = result.getJSONObject("answers");
-                JSONArray answer = answers.getJSONArray("answer");
-                //llenar las opciones de la encuesta
-                for (int i = 0; i < answer.length(); i++) {
-                    JSONObject answerObj = answer.getJSONObject(i);
-                    String text = answerObj.getString("text");
-                    int total = answerObj.getInt("total");
-                    int percent = answerObj.getInt("percent");
-                    Opcion opcion = new Opcion(total, percent, text);
-                    opciones.add(opcion);
-                }
-            } else {
-                System.out.println("Request failed. Response Code: " + responseCode);
-            }
-            // Cerrar la conexión
-            connection.disconnect();
-        } catch (Exception e) {
-            System.out.println("Algo salió mal en el cliente " + e.getMessage());
-            //e.printStackTrace(System.out);
-            System.exit(0);
+            this.connection = (HttpURLConnection) apiUrl.openConnection(); // Abrir la conexión con el objeto URL
+            connection.setRequestMethod("POST"); // Establecer el método de solicitud como POST
+            connection.setRequestProperty("Content-Type", "application/json; utf-8"); // Establecer las propiedades del encabezado de la solicitud
+            connection.setRequestProperty("Accept", "application/json");
+            connection.setDoInput(true); // Habilitar la entrada
+            connection.setDoOutput(true); // Habilitar la salida
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+    }
+
+    // Declarar un método público para enviar el objeto JSON al API
+    public void sendJson(String requestBody) {
+        try {
+            OutputStream os = connection.getOutputStream(); // Obtener el flujo de salida de la conexión
+            byte[] input = requestBody.getBytes("utf-8"); // Convertir el objeto JSON en un arreglo de bytes usando la codificación UTF-8
+            os.write(input, 0, input.length); // Escribir el arreglo de bytes en el flujo de salida
+            os.close(); // Cerrar el flujo de salida
+            // Imprimir el código de respuesta del API
+            //System.out.println("Response code: " + connection.getResponseCode());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // Declarar un método público para recibir la respuesta del API
+    public String getResponse() {
+        StringBuilder response = new StringBuilder(); // Crear un objeto StringBuilder que contenga la respuesta del API
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream(), "utf-8")); // Obtener el flujo de entrada de la conexión y crear un objeto BufferedReader para leerlo usando la codificación UTF-8
+            String responseLine = null;
+            while ((responseLine = br.readLine()) != null) { // Leer cada línea del flujo de entrada hasta que sea nula
+                response.append(responseLine.trim()); // Añadir cada línea al objeto StringBuilder, eliminando los espacios en blanco al principio y al final
+            }
+            br.close();
+            // Cerrar el flujo de entrada
+            JSONObject json = new JSONObject(response.toString());
+            JSONObject pdResponse = json.getJSONObject("pdResponse");
+            JSONObject demands = pdResponse.getJSONObject("demands");
+            JSONArray demand = demands.getJSONArray("demand");
+            JSONObject result = demand.getJSONObject(0).getJSONObject("result");
+            JSONObject answers = result.getJSONObject("answers");
+            JSONArray answer = answers.getJSONArray("answer");
+            //llenar las opciones de la encuesta
+            for (int i = 0; i < answer.length(); i++) {
+                JSONObject answerObj = answer.getJSONObject(i);
+                String text = answerObj.getString("text");
+                int total = answerObj.getInt("total");
+                int percent = answerObj.getInt("percent");
+                //System.out.println("response " + text + " " + total + " " + percent);
+                Opcion opcion = new Opcion(total, percent, text);
+                opciones.add(opcion);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return response.toString(); // Devolver la respuesta del API como una cadena
+    }
+
+    // Declarar un método público para desconectar la conexión
+    public void disconnect() {
+        connection.disconnect(); // Desconectar la conexión
     }
 }
